a5edfc323b59aa38f87f335ba92c79160f72c85d
MatthewBlain02 <matthewblain76@gmail.com>
2023-02-16 20:15:43 +0000

    Tutorial 5 Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-addArticle(title:HTMLInputElement, link:HTMLInputElement):Boolean {
-  console.log(`Adding Article Title ${title.value} And Adding Article Link ${link.value}`);
+
+  articles:Article[];
+
+  constructor(){
+    this.articles=[
+      new Article('Angular', 'http://angular.io', 5),
+      new Article('Google', 'http://google.ie', 3),
+      new Article('TikTok', 'http://tiktok.com', 1)
+    ];
+  }
+  addArticle(title:HTMLInputElement, link:HTMLInputElement):Boolean {
+    this.articles.push(new Article(title.value,link.value,0));
+    title.value='';
+    link.value='';
+  
+    console.log(`Adding Article Title ${title.value} And Adding Article Link ${link.value}`);
   return false;
 }
   title = 'reddit-app';
--- src/app/article/article.model.ts
+++ src/app/article/article.model.ts
@@ -7,7 +7,7 @@ export class Article{
 constructor(title:string, link:string, votes?:number) {
     this.title=title;
     this.link=link;
-    this.votes=votes || 10;
+    this.votes=votes || 0;
 }
 
 voteUp(){
